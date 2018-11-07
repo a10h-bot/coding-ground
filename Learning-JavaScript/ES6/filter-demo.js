ec26c0c231eb46667647b2e439ef72fb13dad0a2
anand <anandp033@gmail.com>
2018-11-08 02:19:10 +0530

    few files modified

---
+++ Learning-JavaScript/ES6/filter-demo.js
@@ -0,0 +1,29 @@
+//Without use of filter
+
+const people = [
+  { name: "John Doe", age: 16 },
+  { name: "Thomas Calls", age: 19 },
+  { name: "Liam Smith", age: 20 },
+  { name: "Jessy Pinkman", age: 18 }
+];
+
+const peopleAbove18 = collection => {
+  let result = [];
+  for (let i = 0; i < collection.length; i++) {
+    let person = collection[i];
+    if (person.age >= 18) {
+      result.push(person);
+    }
+  }
+  return result;
+};
+
+console.log(peopleAbove18(people));
+
+//same code with use of filter
+
+const peopleAbove18New = collection => {
+  return collection.filter(x => x.age >= 18);
+};
+
+console.log(peopleAbove18New(people));
