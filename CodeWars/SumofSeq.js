9aa07a0fb5142fdc1e6e167e521d5c8eaad55bc4
anand <anandp033@gmail.com>
2018-11-17 20:34:17 +0530

    V1

---
+++ CodeWars/SumofSeq.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/586f6741c66d18c22800010a
+let sequenceSum = (begin, end, step) => {
+  if (begin > end) {
+    return 0;
+  }
+  return begin + sequenceSum(begin + step, end, step);
+};
+
+console.log(sequenceSum());
