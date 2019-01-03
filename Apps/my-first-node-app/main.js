b1a03d7ca265d6ab3c1782f6a4fcb5b476c8ab7c
anand <anandp033@gmail.com>
2019-01-03 23:16:11 +0530

    🤓

--- Apps/my-first-node-app/main.js
+++ Apps/my-first-node-app/main.js
@@ -3,7 +3,7 @@ const myDate = require("./mymodule");
 http
   .createServer(function(req, res) {
     res.writeHead(200, { "content-type": "text/html" });
-    res.write(myDate.myDate());
+    res.write(myDate.myDate() + " " + myDate.);
     res.end();
   })
   .listen(8081);
