708afd7efa444af8277c4b88ea0d0bf2e1f4c5cc
anand <anandp033@gmail.com>
2019-01-18 00:30:35 +0530

    added a new Kata

--- CodeWars/SumofSeq.js
+++ CodeWars/SumofSeq.js
@@ -1,10 +1,24 @@
-//https://www.codewars.com/kata/586f6741c66d18c22800010a
-let sequenceSum = (begin, end, step) => {
-  if (begin > end) {
-    return 0;
-  }
-  return begin + sequenceSum(begin + step, end, step);
-};
+// //https://www.codewars.com/kata/586f6741c66d18c22800010a
+// let sequenceSum = (begin, end, step) => {
+//   if (begin > end) {
+//     return 0;
+//   }
+//   return begin + sequenceSum(begin + step, end, step);
+// };
 
-console.log(sequenceSum());
+// console.log(sequenceSum());
 
+let array = [4, 8, 9, 12];
+array.filter(x => x % 2 === 0 ? x : undefined).map(x => 2 * x).forEach(x => console.log(x));
+let acc = array.reduce((acc, ele) => {
+  acc += ele;
+  return acc;
+}, 0);
+
+console.log(acc);
+
+let array1 = [24, 18, 9, 3];
+let x = array1.sort((a, b) => a - b)[0];
+console.log(x);
+let a = [8, 9, 10, 18];
+console.log([8, 9, 10, 18][0]);
--- CodeWars/titleCase.js
+++ CodeWars/titleCase.js
@@ -1,6 +1,24 @@
 //https://www.codewars.com/kata/title-case/train/javascript
+function capitalize(word) {
+  return word[0].toUpperCase() + word.slice(1).toLowerCase();
+}
 
-function titleCase(title, minorWords) {
+function titleCase(title, minorWords = '') {
+  if (title.trim() === '') return "";
+  let checkMinorWords =
+    minorWords.
+  toLowerCase().split(" ").reduce((words, word) => {
+    words[word] = true;
+    return words;
+  }, {});
+  return title
+    .toLowerCase()
+    .split(' ')
+    .map((word, i) => {
+      if (i == 0) return capitalize(word);
+      if (checkMinorWords[word]) return word;
+      return capitalize(word);
+    }).join(' ');
 
 }
 
