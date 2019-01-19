2584270b66f32178fd10c0f17d4b18952ba1ee76
anand <anandp033@gmail.com>
2019-01-19 21:48:48 +0530

    Done

--- JS_files/LearningJS/doc/main.js
+++ JS_files/LearningJS/doc/main.js
@@ -1,7 +1,21 @@
-function greeting() {
-
-  document.getElementById('response').innerHTML = "hi there";
-}
-
-var x = document.querySelector('.div1')
-console.log(x);
+const fs = require('fs');
+const vote = {
+  mongo: 0,
+  mysql: 0,
+  postgres: 0
+};
+fs.readFile('votes.txt', 'utf-8', (err, data) => {
+  if (err) {
+    throw err;
+  }
+  data.toLowerCase().split('\n').forEach(element => {
+    if (element.includes('mongo')) {
+      vote.mongo++;
+    } else if (element.includes('mysql')) {
+      vote.mysql++;
+    } else if (element.includes('postgres')) {
+      vote.postgres++;
+    }
+  })
+  console.log(vote);
+});
